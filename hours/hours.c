9e54fc7e8de78d60f137b6c3085d804f42d35b8c
fry <fry.land@hotmail.fr>
2023-05-13 06:38:51 +0200

    first commit

---
+++ hours/hours.c
@@ -0,0 +1,49 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+float calc_hours(int hours[], int weeks, char output);
+
+int main(void)
+{
+    int weeks = get_int("Number of weeks taking CS50: ");
+    int hours[weeks];
+
+    for (int i = 0; i < weeks; i++)
+    {
+        hours[i] = get_int("Week %i HW Hours: ", i);
+    }
+
+    char output;
+    do
+    {
+        output = toupper(get_char("Enter T for total hours, A for average hours per week: "));
+    }
+    while (output != 'T' && output != 'A');
+
+    printf("%.1f hours\n", calc_hours(hours, weeks, output));
+}
+
+// TODO: complete the calc_hours function
+float calc_hours(int hours[], int weeks, char output)
+{
+    int total_hours = 0;
+    int total = 0;
+    float average = 0;
+
+    for (int i = 0; i < weeks; i++)
+    {
+        total_hours += hours[i];
+    }
+     if (output == 'T')
+    {
+        total = total_hours;
+        return total;
+    }
+    else if (output == 'A')
+    {
+        average = total_hours / (float) weeks;
+        return average;
+    }
+    return 0;
+}
